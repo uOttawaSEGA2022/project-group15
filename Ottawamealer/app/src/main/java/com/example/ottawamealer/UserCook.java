64f24a76cbc7543ba9e7bf71cb1668e6ac7127ae
Anas Hammou <anashammou2003@gmail.com>
2022-10-22 00:08:09 -0400

    added working product

---
+++ Ottawamealer/app/src/main/java/com/example/ottawamealer/UserCook.java
@@ -0,0 +1,14 @@
+package com.example.ottawamealer;
+
+public class UserCook {
+    public String firstName, lastName, email;
+
+    public UserCook() {
+    }
+
+    public UserCook(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
