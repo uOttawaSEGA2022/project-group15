3ac94bff925a53a72222b24362d60066ced001df
Soham <sohamdavenasa@gmail.com>
2022-11-11 21:04:34 -0500

    Upload image button on Register Cook

--- ADOP 2/app/src/main/java/com/example/ottawamealer/RegisterCook.java
+++ ADOP 2/app/src/main/java/com/example/ottawamealer/RegisterCook.java
@@ -1,11 +1,13 @@
 package com.example.ottawamealer;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -23,18 +25,26 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterCook extends AppCompatActivity implements View.OnClickListener {
     private TextView registerCook, registerUser;
-    private ImageView banner;
+    private ImageView banner, image;
     private EditText editTextFirstName, editTextLastName, editTextAddress, editTextEmail, editTextPassword, editTextDescription;
     private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
+    final private int GALLERY_REQUEST_CODE=1000;
+
+    Button uploadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_cook);
 
+        Button uploadButton = findViewById(R.id.uploadButton);
+
+
         mAuth = FirebaseAuth.getInstance();
+
+        image = (ImageView) findViewById(R.id.image);
 
         banner=(ImageView) findViewById(R.id.bannerc);
         banner.setOnClickListener(this);
@@ -54,6 +64,26 @@ public class RegisterCook extends AppCompatActivity implements View.OnClickListe
 
         progressBar = (ProgressBar) findViewById(R.id.progressBarc);
 
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+            if(requestCode == GALLERY_REQUEST_CODE) {
+                image.setImageURI(data.getData());
+            }
+        }
     }
 
     @Override
